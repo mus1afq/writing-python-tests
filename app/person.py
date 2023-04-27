4dbf9b8c14a1f7821eac95271cffaebdea51d99b
mus1afq <mustafaashk@yahoo.com>
2023-04-27 19:17:56 +0100

    feat: wrote test for object methods

--- app/person.py
+++ app/person.py
@@ -5,3 +5,6 @@ class Person:
 
     def __str__(self):
         return f"{self.name}({self.age})"
+
+    def greeting(self):
+        return f"Hello my name is {self.name}"
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,16 @@
 from app.person import Person
 
-#running tests on stringß
+
 def test_person_obj_str(person_1):
+    """Test object string."""
     assert str(person_1) == "John(36)"
 
 
 def test_person_created(person_1):
+    """Test instance."""
     assert isinstance(person_1, Person)
+
+
+def test_person_intro(person_1):
+    """Test object methods."""
+    assert person_1.greeting() == "Hello my name is John"
